d8d159f8052a0777c0367330a99e951c2f09f56a
hrwX <himanshuwarekar@yahoo.com>
2020-10-16 15:34:37 +0530

    fix: rename file

--- erpnext/patches/v12_0/remove_delivery_trip_custom_fields.py
+++ erpnext/patches/v12_0/remove_delivery_trip_custom_fields.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, Frappe and Contributors
+# Copyright (c) 2020, Frappe and Contributors
 # License: GNU General Public License v3. See license.txt
 
 from __future__ import unicode_literals
@@ -7,5 +7,4 @@ import frappe
 def execute():
 
 	for cf in ["Delivery Trip-column_break_17", "Delivery Trip-map_html", "Delivery Trip-odometer", "Delivery Trip-sb_map"]:
-		if frappe.db.exists("Custom Field", cf):
-			frappe.delete_doc_if_exists("Custom Field", cf)
+		frappe.delete_doc_if_exists("Custom Field", cf)
